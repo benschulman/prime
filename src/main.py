25477069fdb8ee594ccebf0540a5ac7892789f93
benschulman <bgschulman31@gmail.com>
2021-11-28 14:29:19 -0600

    updated main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,39 @@
-from prime.constants import MED_BOUND
-from prime.constants import STAR_WITNESSES_XLARGE
+import pandas as pd
+from tqdm import tqdm
+
+from prime.prime import is_prob_prime
 from prime.prime import miller_rabin_prime
+
+# from prime.constants import MED_BOUND
 
 
 def main():
-    print(MED_BOUND)
-    for wit in STAR_WITNESSES_XLARGE:
-        prime = miller_rabin_prime(MED_BOUND, wit)
-        print(f"Witness: {wit}, {prime}")
+    # dict of liars
+    liars = dict()
+    # loop over a large number of value
+    for i in tqdm(range(7, 10000, 2), desc="Loading..."):
+        # get if the number is actually prime
+        prime = is_prob_prime(i)
+        # if the number is not prime find all the witnesses which are liars
+        if not prime:
+            # for every possible witness
+            for wit in range(2, i):
+                # check if the witness says it is prime
+                mr_prime = miller_rabin_prime(i, wit)
+                # if the witness says it's a prime, then it is a liar
+                if mr_prime:
+                    # update liars dict
+                    if wit not in liars:
+                        liars[wit] = 1
+                    else:
+                        liars[wit] += 1
+
+    sorted_items = sorted(
+        liars.items(), key=lambda x: (x[1], -x[0]), reverse=True
+    )
+    df = pd.DataFrame(sorted_items, columns=["Witness", "Liar Count"])
+    print(df.head())
+    df.to_csv("liars.csv", index=False)
 
 
 if __name__ == "__main__":
