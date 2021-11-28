ba497001e7ffe368060c65feb494a8897a9ebca1
benschulman <bgschulman31@gmail.com>
2021-11-28 15:01:21 -0600

    updated main to precompute nonprimes

--- src/main.py
+++ src/main.py
@@ -8,25 +8,27 @@ from prime.prime import miller_rabin_prime
 
 
 def main():
+    not_primes_odds = list()
+    # precompute non-primes (odds):
+    for i in tqdm(range(7, 50000, 2), desc="Pre-computing non-primes..."):
+        if not is_prob_prime(i):
+            not_primes_odds.append(i)
+
     # dict of liars
     liars = dict()
     # loop over a large number of value
-    for i in tqdm(range(7, 10000, 2), desc="Loading..."):
-        # get if the number is actually prime
-        prime = is_prob_prime(i)
-        # if the number is not prime find all the witnesses which are liars
-        if not prime:
-            # for every possible witness
-            for wit in range(2, i):
-                # check if the witness says it is prime
-                mr_prime = miller_rabin_prime(i, wit)
-                # if the witness says it's a prime, then it is a liar
-                if mr_prime:
-                    # update liars dict
-                    if wit not in liars:
-                        liars[wit] = 1
-                    else:
-                        liars[wit] += 1
+    for i in tqdm(not_primes_odds, desc="Calculating Liars..."):
+        # for every possible witness
+        for wit in range(2, i):
+            # check if the witness says it is prime
+            mr_prime = miller_rabin_prime(i, wit)
+            # if the witness says it's a prime, then it is a liar
+            if mr_prime:
+                # update liars dict
+                if wit not in liars:
+                    liars[wit] = 1
+                else:
+                    liars[wit] += 1
 
     sorted_items = sorted(
         liars.items(), key=lambda x: (x[1], -x[0]), reverse=True
